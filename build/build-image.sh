6676fad1f9683ff4e2f136858fa3b673f252a2c0
root <ed.sealing@sealingtech.org>
2017-05-15 22:54:13 -0400

    Initial - Script to build environment - Script to build the Hybrid-ISO image

---
+++ build/build-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+WORKINGDIR=`pwd`
+BUILDDIR=~/build
+rm -f ./EDCOP.iso
+#cd $BUILDDIR/isolinux
+rm -rf $BUILDDIR/isolinux/repodata
+cd $BUILDDIR/isolinux
+createrepo -g ../comps.xml .
+#createrepo -g $BUILDDIR/comps.xml -o $BUILDDIR/isolinux/ $BUILDDIR/isolinux/Packages/
+mkisofs -o EDCOP.iso -J -joliet-long -rational-rock -untranslated-filenames -translation-table -input-charset utf-8 -x ./lost+found -V "EDCOP" -volset "EDCOP" -A "EDCOP" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e images/efiboot.img -no-emul-boot $BUILDDIR/isolinux/
+implantisomd5 EDCOP.iso
+isohybrid -u EDCOP.iso
+mv EDCOP.iso $WORKINGDIR
